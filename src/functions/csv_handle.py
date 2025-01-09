ccdf70426960493b4b9d573ad424c2b03acdea1b
Killian KOPP <killiankopp@gmail.com>
2025-01-09 22:16:26 +0100

    WIP refactoring génération de JSON

--- src/functions/csv_handle.py
+++ src/functions/csv_handle.py
@@ -1,59 +1,50 @@
 import os
 import uuid
+from pathlib import Path
 
 import pandas as pd
 
-from src.functions.file_handle import is_image, list_files
+from src.functions.file_handle import list_files
 from src.functions.folder_handle import list_folders
-from src.functions.img_handle import extract_datetime_from_infos, extract_infos
 
 
-def generate_csv(path: str):
-    folders = list_folders(path)
+def save_to_csv(path_file: Path, listing):
+    df = pd.DataFrame(listing, columns = ['folder', 'file', 'datetime'])
+    df.to_csv(path_file, index = False)
+
+
+def split_csv_files(base_path: str, batch: str):
+    folders = list_folders(base_path)
     for folder in folders:
-        path_folder = f'{path}/{folder}'
-        listing = []
-        files = list_files(folder)
-        for file in files:
-            path_file = f'{path}/{folder}/{file}'
-            if is_image(path_file) is True:
-                infos = extract_infos(folder, file)
-                datetime = extract_datetime_from_infos(infos)
-                listing.append([folder, file, datetime])
-        df = pd.DataFrame(listing, columns = ['folder', 'file', 'datetime'])
-        df.to_csv(path_folder + '/listing.csv', index = False)
-
-
-def split_csv(path: str, batch: str):
-    folders = list_folders(path)
-    for folder in folders:
-        df = pd.read_csv(f'{path}/totreat/{folder}/listing.csv')
+        input_file = Path(base_path) / 'totreat' / folder / 'listing.csv'
+        df = pd.read_csv(input_file)
         if 'date' in df.columns:
             dates = df['date'].unique()
-
             for date in dates:
                 file_name = uuid.uuid4()
-                output_file = f'{path}/photos/{batch}/{date}/{file_name}.csv'
+                output_folder = Path(base_path) / 'photos' / batch / str(date)
+                output_folder.mkdir(parents = True, exist_ok = True)
+                output_file = output_folder / f'{file_name}.csv'
                 df_filtered = df[df['date'] == date]
                 df_filtered.to_csv(output_file, index = False)
 
 
-def merge_csv(path: str):
-    folders = list_folders(path)
+def merge_csv_files(base_path: str):
+    folders = list_folders(base_path)
     for folder in folders:
-        path = f'{path}/{folder}'
+        path = Path(base_path) / folder
         files = list_files(path, '.csv')
-        df = pd.concat([pd.read_csv(f'{path}/{file}') for file in files])
-        df.to_csv(f'{path}/merged.csv', index = False)
+        df = pd.concat([pd.read_csv(path / file) for file in files])
+        output_file = path / 'merged.csv'
+        df.to_csv(output_file, index = False)
 
 
-def generate_filename():
-    base = '../artefacts/totreat'
-    folders = list_folders(base)
+def generate_filename(base_path: str):
+    folders = list_folders(base_path)
     for folder in folders:
-        path = f'{base}/{folder}/listing.csv'
-        df = pd.read_csv(path)
+        input_file = Path(base_path) / folder / 'listing.csv'
+        df = pd.read_csv(input_file)
         df["datetime"] = pd.to_datetime(df["datetime"])
         df["date"] = df["datetime"].dt.date
-        df["uuid"] = [f"{uuid.uuid4()}{os.path.splitext(file)[1].lower()}" for file in df["file"]]
-        df.to_csv(path, index = False)
+        df["uuid"] = [f"{str(uuid.uuid4())}{os.path.splitext(file)[1].lower()}" for file in df["file"]]
+        df.to_csv(input_file, index = False)
--- src/functions/img_handle.py
+++ src/functions/img_handle.py
@@ -12,3 +12,18 @@ def extract_datetime_from_infos(infos: dict) -> str:
     if infos.has_exif and hasattr(infos, "datetime"):
         date_time = datetime.strptime(infos.datetime, "%Y:%m:%d %H:%M:%S")
         return date_time
+
+
+def process_images(path_img_src: Path):
+    img_src = Path(path_img_src)
+    folders = list_folders(img_src)
+    for folder in folders:
+        folder_path = Path(base_path) / folder
+        listing = []
+        files = list_files(folder_path)
+        for file in files:
+            if is_image(file):
+                infos = extract_infos(folder, file)
+                datetime = extract_datetime_from_infos(infos)
+                listing.append([folder, str(file), datetime])
+        save_listing(folder_path, listing)
